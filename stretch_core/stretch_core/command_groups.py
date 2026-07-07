afec91f87407fa69856f0f3a6e87a62f13598057
Anna <annagarverick@hello-robot.com>
2026-07-07 12:27:14 -0400

    Revert "Use updated RobotJoint utils for pg4 and sg4 support"
    This reverts commit a9e252f2fab9db20892b93b0b5f27286e4c68dcb.

--- stretch_core/stretch_core/command_groups.py
+++ stretch_core/stretch_core/command_groups.py
@@ -181,7 +181,8 @@ class ParallelGripperCommandGroup(BaseCommandGroup):
     @check_active()
     def queue_execution(self, robot: Any, **kwargs: Any) -> None:
         goal_m = self.goal['position']
-        robot.end_of_arm.move_to('parallel_gripper', goal_m, self.goal['velocity'], self.goal['acceleration'])
+        goal_mm = goal_m * 1000.0
+        robot.end_of_arm.move_to_mm('parallel_gripper', goal_mm, self.goal['velocity'], self.goal['acceleration'])
 
     @override
     @check_active()
@@ -196,7 +197,7 @@ class ParallelGripperCommandGroup(BaseCommandGroup):
     @override
     @check_active()
     def cancel_execution(self, robot: Any, **kwargs: Any) -> None:
-        robot.end_of_arm.move_by('parallel_gripper', 0.0)
+        robot.end_of_arm.move_by_mm('parallel_gripper', 0.0)
 
     @override
     @check_active()
--- stretch_core/stretch_core/stretch_driver.py
+++ stretch_core/stretch_core/stretch_driver.py
@@ -34,8 +34,7 @@ import tf2_ros
 from tf_transformations import quaternion_from_euler
 
 import hello_helpers.joy_conversion as jc
-from stretch4_body.core.robot_params import nominal_system_params
-from stretch4_body.utils.stretch_pose_models import RobotJoints
+
 
 class StretchDriver(Node):
 
@@ -285,8 +284,8 @@ class StretchDriver(Node):
         before the robot stops moving. It cannot exceed velocity_timeout parameter.
         """
         with self.driver_mode_lock:
-            if self.driver_mode not in ['velocity', 'navigation']:
-                self.robot.logger.warn(f'Must be in velocity or navigation mode to service JointJog msg. Current mode = {self.driver_mode}.')
+            if self.driver_mode != 'velocity':
+                self.robot.logger.warn(f'Must be in velocity mode to service JointJog msg. Current mode = {self.driver_mode}.')
                 return
 
         # Queue velocity commands
@@ -297,14 +296,10 @@ class StretchDriver(Node):
 
             acceleration_param = self.get_parameter_or(f"joint_acceleration.{joint.split("_joint")[0]}",None).value
 
-            velocity_val = jointjog_msg.velocities[i]
-            if "gripper" in joint:
-                joint_clean = joint.split('_joint')[0]
-                joint_enum = RobotJoints.get_joint_by_name(joint_clean)
-                if joint_enum:
-                    velocity_val = joint_enum.urdf_to_subsystem(velocity_val)
+            if "gripper" in joint: 
+                jointjog_msg.velocities[i] *= 300
 
-            self.set_vel_functions[joint](velocity_val, acceleration_param)
+            self.set_vel_functions[joint](jointjog_msg.velocities[i], acceleration_param)
 
         # Set timeout (TODO)
         self.robot.logger.debug(str(self.robot.cmd_dict))
