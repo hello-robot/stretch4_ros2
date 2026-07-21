0c5e21bb7e0030cd44c588bb6ba63bb9909b3d58
Ola Ghattas <oghattas@hello-robot.com>
2026-07-20 17:40:48 -0700

    add map server to mapping

--- stretch_nav2/launch/offline_mapping.launch.py
+++ stretch_nav2/launch/offline_mapping.launch.py
@@ -1,7 +1,18 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import (
+    DeclareLaunchArgument,
+    EmitEvent,
+    IncludeLaunchDescription,
+    LogInfo,
+    RegisterEventHandler,
+)
+from launch.events import matches_action
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
 from launch_ros.substitutions import FindPackageShare
+from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
@@ -40,10 +51,49 @@ def generate_launch_description():
         launch_arguments={'use_rviz': LaunchConfiguration('use_rviz')}.items(),
     )
 
+    # Expose nav2's SaveMap service 
+    map_saver = LifecycleNode(
+        package='nav2_map_server',
+        executable='map_saver_server',
+        name='map_saver',
+        namespace='',
+        output='screen',
+        parameters=[{
+            'save_map_timeout': 5.0,
+            'free_thresh_default': 0.25,
+            'occupied_thresh_default': 0.65,
+        }],
+    )
+
+    configure_map_saver = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(map_saver),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    activate_map_saver = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=map_saver,
+            start_state='configuring',
+            goal_state='inactive',
+            entities=[
+                LogInfo(msg='[map_saver] configured; activating.'),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(map_saver),
+                    transition_id=Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+        )
+    )
+
     return LaunchDescription([
         tool_preset_arg,
         use_rviz_arg,
         stretch_driver_launch,
         lidar_launch,
-        slam_toolbox_launch
+        slam_toolbox_launch,
+        map_saver,
+        activate_map_saver,
+        configure_map_saver,
     ])
