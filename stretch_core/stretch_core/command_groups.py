636f69d510066d6fb4eacaa1b14b60b82c138be8
Anna <annagarverick@hello-robot.com>
2026-08-19 18:23:35 -0400

    Swotch to ToolMetadata

--- stretch_core/stretch_core/command_groups.py
+++ stretch_core/stretch_core/command_groups.py
@@ -183,8 +183,7 @@ class ParallelGripperCommandGroup(BaseCommandGroup):
     @override
     @check_active()
     def queue_execution(self, robot: Any, **kwargs: Any) -> None:
-        goal_m = self.goal['position']
-        robot.end_of_arm.move_to('parallel_gripper', goal_m, self.goal['velocity'], self.goal['acceleration'])
+        robot.end_of_arm.move_to(RobotJoints.gripper.value, self.goal['position'], self.goal['velocity'], self.goal['acceleration'])
 
     @override
     @check_active()
@@ -199,7 +198,7 @@ class ParallelGripperCommandGroup(BaseCommandGroup):
     @override
     @check_active()
     def cancel_execution(self, robot: Any, **kwargs: Any) -> None:
-        robot.end_of_arm.move_by('parallel_gripper', 0.0)
+        robot.end_of_arm.move_by(RobotJoints.gripper.value, 0.0)
 
     @override
     @check_active()
